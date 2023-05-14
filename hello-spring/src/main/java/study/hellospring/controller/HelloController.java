0bc3a5b997828d5070751d6ab2c62d2783b73afb
beeguriri <kkamboting@gmail.com>
2023-05-14 17:51:04 +0900

    API방식

--- hello-spring/src/main/java/study/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/study/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,4 +24,39 @@ public class HelloController {
         //템플릿엔진이 HTML로 변환 후 웹브라우저에 넘겨줌
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody //응답 body 에 return 부분이 직접 들어감 (뷰 없음)
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    // ResponseBody: http 바디에 바로 데이터 반환(객체 반환하면 => default 가 json!)
+    // httpMessageConverter 가 동작
+    // 기본문자 : StringHttpMessageConverter
+    // 기본객체 : MappingJackson2HttpMessageConverter
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        //json 형태로 return (key: "name":, value: name)
+        return hello;
+    }
+
+    static class Hello {
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
 }
