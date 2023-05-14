a953105ee5a28fb81f78be7e3abe950c943cc292
beeguriri <kkamboting@gmail.com>
2023-05-14 17:30:32 +0900

    MVC와 템플릿 엔진

--- hello-spring/src/main/java/study/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/study/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package study.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,15 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "spring!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        //url 입력할때 ?name="값" 넣어주면 값이 넘어옴
+        model.addAttribute("name", name);
+
+        //뷰리졸버가 resources>templates>hello-template.html 파일 매핑
+        //템플릿엔진이 HTML로 변환 후 웹브라우저에 넘겨줌
+        return "hello-template";
     }
 }
