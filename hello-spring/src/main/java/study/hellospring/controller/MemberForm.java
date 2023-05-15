9f209e16fa6c17023c0db8aabae2eb63436dbd93
beeguriri <kkamboting@gmail.com>
2023-05-15 10:01:00 +0900

    0515 회원가입, 회원조회 만들기

---
+++ hello-spring/src/main/java/study/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package study.hellospring.controller;
+
+public class MemberForm {
+
+    private String name; //form의 name과 matching
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
