ad5bea09aeecc634a64c2e670e232308add741af
beeguriri <kkamboting@gmail.com>
2023-05-20 17:10:43 +0900

    각 역할과 구현클래스를 잘 나타낼 수 있게 AppConfig 리팩토링

--- basic/src/main/java/study/core/AppConfig.java
+++ basic/src/main/java/study/core/AppConfig.java
@@ -1,6 +1,8 @@
 package study.core;
 
+import study.core.discount.DiscountPolicy;
 import study.core.discount.FixDiscountPolicy;
+import study.core.member.MemberRepository;
 import study.core.member.MemberService;
 import study.core.member.MemberServiceImpl;
 import study.core.member.MemoryMemberRepository;
@@ -9,12 +11,21 @@ import study.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    // 각 역할과 구현클래스를 잘 나타낼 수 있게 리팩토링
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(),
-                                        new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
