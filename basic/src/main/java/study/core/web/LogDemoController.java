4c773fee8b187135846f96fa9f0a911580e31260
beeguriri <kkamboting@gmail.com>
2023-05-21 21:47:43 +0900

    ObjectProvider, proxyMode 사용해서 빈 필요시점까지 지연

--- basic/src/main/java/study/core/web/LogDemoController.java
+++ basic/src/main/java/study/core/web/LogDemoController.java
@@ -14,18 +14,20 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
 
         String requestURL = request.getRequestURL().toString();
         //필요한 시점에 주입받아서 사용
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000); //리퀘스트 종료될때까지 uuid 살아있는거 확인
         logDemoService.logic("testId");
 
         return "OK";
