87aba1bedb9a9a6e67bf875c5f91e28d03d02df3
beeguriri <kkamboting@gmail.com>
2023-05-29 17:40:21 +0900

    로그인, 로그아웃 체인 수정

--- security-study/src/main/java/beeguri/securitystudy/config/SpringSecurityConfig.java
+++ security-study/src/main/java/beeguri/securitystudy/config/SpringSecurityConfig.java
@@ -2,8 +2,6 @@ package beeguri.securitystudy.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,8 +10,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터 체인에 등록
-//secured annotation 활성화, PreAuthorize annotation 활성화 (해당 메서드만 인증필요 할때)
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SpringSecurityConfig {
 
     @Bean
@@ -22,56 +18,20 @@ public class SpringSecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-//                .antMatchers("/user/**").authenticated() //인증 필요
-////                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')") //인증 필요 && 권한필요
-////                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") //인증 필요 && 권한필요
-////                .anyRequest().permitAll()
-                .antMatchers("/api/join", "/api/login").permitAll()
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
+                .antMatchers("/", "/api/user/join", "/api/user/login").permitAll()
+                // 그 외 나머지 요청은 모두 인증된 회원만 접근 가능
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-    //                .loginPage("/loginForm") // 인증이 필요한 페이지의 경우 로그인이 안되어있으면 해당 페이지로 넘어옴
-                    .loginProcessingUrl("/login") //login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줌
-//                    .defaultSuccessUrl("/")
-                .and()
                 .logout()
-//                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("http://localhost:3000")
-                    .invalidateHttpSession(true);
-//                .and()
-//                .oauth2Login()
-//                .loginPage("/login")
-//                .userInfoEndpoint()
-//                .userService(principalOauth2UserService);
+                .logoutRequestMatcher(new AntPathRequestMatcher("/api/user/logout"))
+                .logoutSuccessUrl("http://localhost:3000");
 
         return http.build();
     }
-
-
-//    @Bean
-//    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//        http
-//            .csrf().disable()
-////                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-////                .and()
-//            .authorizeRequests()
-//                    .antMatchers("/", "/api/login").permitAll()
-//                    .anyRequest().authenticated()
-//            .and()
-//            .logout()
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                .logoutSuccessUrl("http://localhost:3000")
-//                .invalidateHttpSession(true);
-//
-//        return http.build();
-//    }
-
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring().antMatchers("/static/js/**","/static/css/**","/static/img/**","/static/frontend/**");
-//    }
-
 }
