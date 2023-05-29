5a4bb013b55e37f382708b7581980a632242d308
beeguriri <kkamboting@gmail.com>
2023-05-29 17:38:24 +0900

    리액트 홈화면 수정

--- security-study/security-study-front/src/pages/Home.js
+++ security-study/security-study-front/src/pages/Home.js
@@ -1,48 +1,44 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
 
-    const [isLogin, setIsLogin] = useState(false);
+    const onClickLogout = async () => {
+        await axios
+            .get("/api/user/logout", {
+            })
+            .then((response) => {
+                console.log(response)
+                sessionStorage.removeItem("isLoggedIn")
+                window.location.href = "/";
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        // sessionStorage.removeItem("isLoggedIn")
 
-    useEffect(() => {
-
-        if (sessionStorage.getItem("user_id") === null) {
-            // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 없다면
-            console.log("isLogin ?? :: ", isLogin);
-        } else {
-            // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 있다면
-            // 로그인 상태 변경
-            setIsLogin(true);
-            console.log("isLogin ?? :: ", isLogin);
-        }
-    });
-
-    const logoutEvent = () => {
-        sessionStorage.removeItem("user_id")
-        setIsLogin(false);
     }
-    
+
     return (
-        // <div>
-        //     홈화면 <br />
-        //     {isLogin ? (
-        //         <ul>
-        //             <li><Link to="/logout" onClick={logoutEvent}>로그아웃</Link></li>
-        //         </ul>
-        //     ) : (
-        //         <ul>
-        //             <li><Link to="/login">로그인</Link></li>
-        //             <li><Link to="/join">회원가입</Link></li>
-        //             <li><Link to="/success">로그인여부 확인</Link></li>
-        //         </ul>
-        //     )}
-        // </div>
         <div>
-            
+            홈화면 입니다!
+            {sessionStorage.getItem("isLoggedIn") ? (
+                <div>
+                    {sessionStorage.getItem("isLoggedIn")}
+                    <form className="loginform" onSubmit={onClickLogout}>
+                        <button
+                            type="submit"
+                        >로그아웃</button>
+                    </form>
+                    <Link to="/join">회원가입</Link>
+                </div>) : (
+                <div>
+                    <Link to="/login">로그인</Link>
+                    <br />
+                    <Link to="/join">회원가입</Link>
+                </div>
+            )}
         </div>
-
-
     )
 }
 
