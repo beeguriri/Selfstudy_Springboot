78c9db335e6986dd7c5c17658e819511659bd865
beeguriri <kkamboting@gmail.com>
2023-05-31 12:52:57 +0900

    주문 리포지토리 및 서비스 개발

---
+++ jpashop/src/main/java/study/jpashop/repository/OrderRepository.java
@@ -0,0 +1,27 @@
+package study.jpashop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import study.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+
+    public Order findOne(Long id){
+        return em.find(Order.class, id);
+    }
+
+    //검색기능 => 동적쿼리
+//    public List<Order> findAll(OrderSearch orderSearch) {}
+
+}
