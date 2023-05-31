ca94d9f3ea51d1f8280cd5150609357a9b7d682a
beeguriri <kkamboting@gmail.com>
2023-05-31 15:58:18 +0900

    주문 도메인 개발

--- jpashop/src/main/java/study/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/study/jpashop/repository/OrderRepository.java
@@ -2,9 +2,13 @@ package study.jpashop.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 import study.jpashop.domain.Order;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,7 +25,81 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //검색기능 => 동적쿼리
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+    //검색기능 => 동적쿼리(검색파라미터 유무에 따라)
+    //Querydsl로 처리
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+        return null;
+    }
+
+    //jpa가 표준으로 제공하는 criteria 동적쿼리 => 이거도 잘 안씀 ...(유지보수가 힘듦)
+    public List<Order> findAllByCriteria(OrderSearch orderSearch){
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        //주문 상태 검색
+        if(orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        //회원 이름 검색
+        if(StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+
+        return query.getResultList();
+    }
+
+    //스트링 타입으로 동적쿼리 생성하는 방법은
+    //버그 잡기도 어렵고 코드가 길어져서 잘 안씀
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())){
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null)
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+
+        if(StringUtils.hasText(orderSearch.getMemberName()))
+            query = query.setParameter("name", orderSearch.getMemberName());
+
+        return query.getResultList();
+    }
 
 }
