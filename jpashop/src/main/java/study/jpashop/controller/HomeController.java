05cbd56b21d7468918055be675a2ab27489be9cd
beeguriri <kkamboting@gmail.com>
2023-05-31 22:27:32 +0900

    홈화면 제어

--- jpashop/src/main/java/study/jpashop/controller/HomeController.java
+++ jpashop/src/main/java/study/jpashop/controller/HomeController.java
@@ -1,11 +1,8 @@
 package study.jpashop.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.logging.Logger;
 
 @Controller
 @Slf4j
