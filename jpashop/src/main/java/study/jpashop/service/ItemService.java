af0dea149640231152c08d3b6d93c69b13adada2
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-06-04 17:24:43 +0900

    0604 jpa1 (#13)
    * css 수정
    
    * 아이템 수정 _ 변경감지와 병합
    
    * 아이템 수정 _ 변경감지와 병합
    
    * 주문_생성, 수정
    
    * update README.md

--- jpashop/src/main/java/study/jpashop/service/ItemService.java
+++ jpashop/src/main/java/study/jpashop/service/ItemService.java
@@ -20,6 +20,20 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    // 변경감지 기능 사용
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stock){
+
+        Item findItem = itemRepository.findOne(itemId);
+
+        // 왠만하면 setter 쓰지말고
+        // findItem.change(name, price, stock) 같은 메서드 만들어쓰자
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stock);
+
+    } // transactional 에 의해 커밋
+
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
