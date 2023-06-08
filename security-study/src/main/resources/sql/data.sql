067de57fa392b8d41764c99c44e456a6ce2b6766
beeguriri <kkamboting@gmail.com>
2023-06-08 14:45:34 +0900

    실행 시 에러 생겨서 잠시 주석 처리 해 놓음

--- security-study/src/main/resources/sql/data.sql
+++ security-study/src/main/resources/sql/data.sql
@@ -1,2 +1,2 @@
-insert into member(userid, password, roles) values ('beeguri', '1234', 'ADMIN');
-insert into member(userid, password, roles) values ('user', '1234', 'USER');
+-- insert into member(userid, password, roles) values ('beeguri', '1234', 'ADMIN');
+-- insert into member(userid, password, roles) values ('user', '1234', 'USER');
