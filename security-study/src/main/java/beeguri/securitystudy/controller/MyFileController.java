eb4cc3753c6deb3b7c753196092c341550060df2
beeguriri <kkamboting@gmail.com>
2023-06-08 14:51:13 +0900

    user 정보와 file 정보 함께 받기

--- security-study/src/main/java/beeguri/securitystudy/controller/MyFileController.java
+++ security-study/src/main/java/beeguri/securitystudy/controller/MyFileController.java
@@ -1,13 +1,16 @@
 package beeguri.securitystudy.controller;
 
+import beeguri.securitystudy.dto.FileDto;
+import beeguri.securitystudy.dto.MemberLoginDto;
 import beeguri.securitystudy.service.MyFileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -16,20 +19,27 @@ public class MyFileController {
     private final MyFileService multipartService;
 
     @PostMapping("/api/uploadFiles")
-    public ResponseEntity<Object> uploadFiles(MultipartFile[] multipartFiles) {
+//    public ResponseEntity<Object> uploadFiles(MultipartFile[] multipartFiles) {
+    public ResponseEntity<Object> uploadFiles(
+            @RequestPart(value = "userid") FileDto userid,
+            @RequestPart(value = "multipartFiles") MultipartFile[] multipartFiles) {
 
         Long fileListId;
+
         try{
-            fileListId = multipartService.uploadFiles(multipartFiles);
+            System.out.println(userid.getUserid());
+            System.out.println(multipartFiles.toString());
+            fileListId = multipartService.uploadFiles(userid.getUserid(), multipartFiles);
 
         } catch (Exception e) {
             System.out.println("에러?");
             return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
         }
 
-        System.out.println(ResponseEntity.ok().body(multipartService.test(fileListId)).toString());
+//        System.out.println(ResponseEntity.ok().body(multipartService.test(fileListId)).toString());
 
         return ResponseEntity.ok().body(multipartService.test(fileListId));
+//        return null;
 
     }
 }
