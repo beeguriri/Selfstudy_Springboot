7c40e9bb91fab039b656a62d36308495cbab0f65
beeguriri <kkamboting@gmail.com>
2023-06-08 14:46:10 +0900

    파일 전송 시 userid 포함해서 전송

--- security-study/security-study-front/src/pages/FileUpload.js
+++ security-study/security-study-front/src/pages/FileUpload.js
@@ -16,18 +16,28 @@ const FileUpload = () => {
     const onFileUpload = () => {
         const formData = new FormData();
 
+        // JSON 형식으로 파싱 후 추가
+        formData.append('userid', new Blob([JSON.stringify(sessionStorage.getItem('userid'))] , {type: "application/json"})); 
+
         fileList.forEach((file) => {
+
             formData.append('multipartFiles', file);
         })
 
-        axios.post('api/uploadFiles', formData)
-        .then((response) => {
+        axios({
+            method: "POST",
+            url: `api/uploadFiles`,
+            mode: "cors",
+            headers: {
+              "Content-Type": "multipart/form-data", // Content-Type을 반드시 이렇게 하여야 한다.
+            },
+            data: formData,
+
+        }).then((response) => {
             console.log("response 왔어용")
             console.log(response.data)
-        });
-
+        })
     }
-
 
     return (
         <div>
