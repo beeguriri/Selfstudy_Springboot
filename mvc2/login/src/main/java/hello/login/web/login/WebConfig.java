e34524396331d3a0aedc894a0fade94b30d146bf
beeguriri <kkamboting@gmail.com>
2023-06-11 17:44:27 +0900

    로그인 - 스프링 인터셉터 - 인증 체크

--- mvc2/login/src/main/java/hello/login/web/login/WebConfig.java
+++ mvc2/login/src/main/java/hello/login/web/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login.web.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**") //모든 경로 허용
+                .excludePathPatterns("/", "/members/add", "/login", "/logout",
+                                            "/css/**", "/*.ico", "/error"); //허용하지않을 경로
     }
 
 //    @Bean
@@ -34,7 +41,7 @@ public class WebConfig implements WebMvcConfigurer {
        return filterRegistrationBean;
     }
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean loginCheckFilter() {
 
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<Filter>();
