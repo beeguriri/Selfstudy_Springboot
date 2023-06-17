5d2e393f2e85b62cbe88490a4c84ec830fb9ce5b
beeguriri <kkamboting@gmail.com>
2023-06-17 15:06:49 +0900

    포맷터 적용해보기

--- mvc2/typeconverter/src/main/java/study/typeconverter/WebConfig.java
+++ mvc2/typeconverter/src/main/java/study/typeconverter/WebConfig.java
@@ -7,15 +7,20 @@ import study.typeconverter.converter.IntegerToStringConverter;
 import study.typeconverter.converter.IpPortToStringConverter;
 import study.typeconverter.converter.StringToIntegerConverter;
 import study.typeconverter.converter.StringToIpPortConverter;
+import study.typeconverter.formatter.MyNumberFormatter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        //우선순위(converter > formatter)때문에 주석처리
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
