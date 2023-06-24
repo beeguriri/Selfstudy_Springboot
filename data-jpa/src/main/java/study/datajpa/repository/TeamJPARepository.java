c65d926a445deed855104073f67c38a5601b28f9
beeguriri <kkamboting@gmail.com>
2023-06-24 11:55:08 +0900

    리팩토링

--- data-jpa/src/main/java/study/datajpa/repository/TeamJPARepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/TeamJPARepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class TeamRepository {
+public class TeamJPARepository {
 
     @PersistenceContext
     private EntityManager em;
