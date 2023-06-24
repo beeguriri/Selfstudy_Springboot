eba6c8d36f5bce20265c909037a6a5255d1fc962
beeguriri <kkamboting@gmail.com>
2023-06-24 10:58:44 +0900

    예제 도메인 모델과 테스트

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,27 +1,42 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+//JPA 표준스펙에 entity 의 default 생성자 필요 (protected)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//연관관계필드는 toString 안하는게 좋음
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    //JPA 표준스펙에 entity 의 default 생성자 필요 (protected)
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY) //x to one 은 반드시 지연로딩
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team!=null)
+            changeTeam(team);
+    }
+
+    //연관관계 setting
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
