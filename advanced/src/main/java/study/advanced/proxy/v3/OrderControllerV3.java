07a5958c1c3e1938b9cc8309e77b2b1df4bb2377
beeguriri <kkamboting@gmail.com>
2023-07-31 12:47:53 +0900

    v3. 컴포넌트 스캔으로 스프링빈 자동 등록

---
+++ advanced/src/main/java/study/advanced/proxy/v3/OrderControllerV3.java
@@ -0,0 +1,25 @@
+package study.advanced.proxy.v3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/proxy")
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+
+    @GetMapping("/v3/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v3/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
