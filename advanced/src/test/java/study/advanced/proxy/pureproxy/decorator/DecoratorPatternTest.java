c6913ade7a90202c6610d711fb514cc3da1f6f62
beeguriri <kkamboting@gmail.com>
2023-07-31 14:31:46 +0900

    데코레이터 패턴 체이닝 테스트

--- advanced/src/test/java/study/advanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ advanced/src/test/java/study/advanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,10 +2,7 @@ package study.advanced.proxy.pureproxy.decorator;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import study.advanced.proxy.pureproxy.decorator.code.Component;
-import study.advanced.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import study.advanced.proxy.pureproxy.decorator.code.MessageDecorator;
-import study.advanced.proxy.pureproxy.decorator.code.RealComponent;
+import study.advanced.proxy.pureproxy.decorator.code.*;
 
 @Slf4j
 public class DecoratorPatternTest {
@@ -25,6 +22,17 @@ public class DecoratorPatternTest {
         Component realComponent = new RealComponent();
         Component messageDecorator = new MessageDecorator(realComponent);
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        //client -> timeDeco -> messageDeco -> realComponent
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
 
         client.execute();
     }
