6d0d6ad46a05672b085aad03e9dd0384ed1c1ae3
beeguriri <kkamboting@gmail.com>
2023-07-31 19:41:34 +0900

    JDK 동적프록시 적용

--- advanced/src/main/java/study/advanced/AdvancedApplication.java
+++ advanced/src/main/java/study/advanced/AdvancedApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import study.advanced.config.v1_proxy.ConcreteProxyConfig;
+import study.advanced.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import study.advanced.trace.logtrace.LogTrace;
 import study.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "study.advanced.proxy")
 public class AdvancedApplication {
 
