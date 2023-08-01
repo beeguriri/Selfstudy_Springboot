186b63ef0d1eb936c41ee3a50b47c414f252fc6c
beeguriri <kkamboting@gmail.com>
2023-08-01 15:27:52 +0900

    `@Aspect`로 어드바이저 생성

--- advanced/src/main/java/study/advanced/AdvancedApplication.java
+++ advanced/src/main/java/study/advanced/AdvancedApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import study.advanced.config.v5_autoproxy.AutoProxyConfig;
+import study.advanced.config.v6_aop.AopConfig;
 import study.advanced.trace.logtrace.LogTrace;
 import study.advanced.trace.logtrace.ThreadLocalLogTrace;
 
@@ -16,7 +16,8 @@ import study.advanced.trace.logtrace.ThreadLocalLogTrace;
 //@Import(ProxyFactoryConfigV1.class)
 //@Import(ProxyFactoryConfigV2.class)
 //@Import(BeanPostProcessorConfig.class)
-@Import(AutoProxyConfig.class)
+//@Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "study.advanced.proxy")
 public class AdvancedApplication {
 
---
+++ advanced/src/main/java/study/advanced/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,39 @@
+package study.advanced.config.v6_aop.aspect;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import study.advanced.trace.TraceStatus;
+import study.advanced.trace.logtrace.LogTrace;
+
+@Slf4j
+@Aspect
+@RequiredArgsConstructor
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    @Around("execution(* study.advanced.proxy..*(..)) && !execution(* study.advanced.proxy..noLog(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        TraceStatus status = null;
+
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            //로직 호출
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+
+            return result;
+
+        } catch(Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
