d6d9c5f9b5ab38641ad32e9bd0420a4710aee1f7
beeguriri <kkamboting@gmail.com>
2023-08-02 17:21:06 +0900

    로그출력 AOP 적용 테스트

--- advanced/src/main/java/study/advanced/aop/exam/ExamService.java
+++ advanced/src/main/java/study/advanced/aop/exam/ExamService.java
@@ -2,6 +2,7 @@ package study.advanced.aop.exam;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import study.advanced.aop.exam.annotation.Trace;
 
 @Service
 @RequiredArgsConstructor
@@ -9,6 +10,7 @@ public class ExamService {
 
     private final ExamRepository examRepository;
 
+    @Trace
     public void request(String itemId){
         examRepository.save(itemId);
     }
